4fbd0c93a0febff1845c6a4c2b15ded35215ef8d
Sterling McLeod <sterlingmcleod9@gmail.com>
2015-02-10 23:38:10 -0500

    Changed euler function to take in an arbitrary first-order derivative

--- diff_eq.hs
+++ diff_eq.hs
@@ -1,11 +1,12 @@
+-- Some practice with Haskell and differential equations
 
 data Point = Point {pointx :: Float, pointy :: Float} deriving (Show)
 
-euler :: Point -> Float -> Float -> Point 
-euler (Point {pointx = x, pointy = y}) h x_end = 
+euler :: (Float -> Float) -> Point -> Float -> Float -> Point 
+euler f' (Point {pointx = x, pointy = y}) h x_end = 
   if x == x_end then (Point x y) 
   else
-    euler (Point (x+h) (((f_prime x) * h) + y)) h x_end
+    euler f' (Point (x+h) (((f' x) * h) + y)) h x_end
 
 
 
