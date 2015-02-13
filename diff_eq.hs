19331e78926455db626cc88f8bf158c21700a6ee
Sterling McLeod <sterlingmcleod9@gmail.com>
2015-02-13 18:18:49 -0500

    Added a main function to diff_eq

--- diff_eq.hs
+++ diff_eq.hs
@@ -1,10 +1,13 @@
+import System.Environment (getArgs)
+
 -- Some practice with Haskell and differential equations
 
-data Point = Point {pointx :: Float, pointy :: Float} deriving (Show)
+
+data Point = Point {px :: Float, py :: Float} deriving (Show)
 
 euler :: (Float -> Float) -> Point -> Float -> Float -> Point 
-euler f' (Point {pointx = x, pointy = y}) h x_end = 
-  if x == x_end then (Point x y) 
+euler f' (Point {px = x, py = y}) h x_end = 
+  if x >= x_end then (Point x y) 
   else
     euler f' (Point (x+h) (((f' x) * h) + y)) h x_end
 
@@ -12,4 +15,12 @@ euler f' (Point {pointx = x, pointy = y}) h x_end =
 
 f_prime :: Float -> Float
 f_prime x = 
-    x + 2
+    3*x*x + 2*x + 1
+
+
+main = do
+    args <- getArgs
+
+    let a = (Point 1 1)
+
+    print $ euler f_prime a 1 100000
