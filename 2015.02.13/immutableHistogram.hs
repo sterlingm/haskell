ac3dc3505a2692d179bae0818b411bab5165f628
Sean Gallagher <stgallag@gmail.com>
2015-02-13 09:41:41 -0500

    added comments

--- 2015.02.13/immutableHistogram.hs
+++ 2015.02.13/immutableHistogram.hs
@@ -13,11 +13,17 @@ import qualified Data.Map as H
 main = do
     path:_ <- getArgs
     withFile path ReadWriteMode $ \handle -> do
+        -- Read the file
         content <- hGetContents handle
+        -- Convert it to lines
         let content_lines = T.lines content
+            -- Convert each line to words
             lines_words = map T.words content_lines
+            -- Make a function to count the words in a line
             count_line = foldl' (\hist word -> H.insertWith' (+) word 1 hist) H.empty
+            -- Count the words in all the lines
             lines_hists = map count_line lines_words
+            -- Merge the results from all of the counts
             finished_hist = H.unionsWith (+) lines_hists
-        
+        -- Print out the finished result
         print $ H.toList finished_hist
